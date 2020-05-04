e2964ccf5f538984c910aef2a74ea6ec884381e3
Zoe Zilz <zoelzilz@gmail.com>
2020-05-04 16:02:02 -0700

    renamed some stuff, made some progress debugging first consumer/resource group. pred/prey half of model still wonky

--- build_paramvals.R
+++ build_paramvals.R
@@ -1,21 +1,20 @@
 ####### BUILDING THE MODEL OUT ONE PIECE AT A TIME ########
 
 # input of algae
-i <- 5
+inp <-5  #5 #or 10 # or 15
+
 # death rates d
-## long et al has them all equal except parasites
-d_A <- 0.05   # death of algae
+d_A <- 0.1   # death of algae
 d_P <- 0.1    # death of parasites
-d_FU <- d_A   # death of uninfected first int hosts
-d_FI <- d_A   # death of infected first int hosts
-d_DU <- d_A   # death of uninfected definitive hosts
-d_DI <- d_A   # death of infected definitive hosts
+d_FU <- 0.05   # death of uninfected first int hosts
+d_FI <- 0.05   # death of infected first int hosts
+d_DU <- 0.05   # death of uninfected definitive hosts
+d_DI <- 0.05   # death of infected definitive hosts
 
 # grazing rate 
-## long et al has these equal to predation rates at first
 ### later I will change them so that infected snails graze at a higher rate
-gamma_I <- 0.3  # grazing rate of infected first int hosts
-gamma_U <- 0.3  # grazing rate of uninfected first int hosts
+gamma_I <- 0.5  # grazing rate of infected first int hosts
+gamma_U <- 0.5  # grazing rate of uninfected first int hosts
 
 # attack rate
 ## these will need to vary later but for now they are all equal (system with no behavioral manipulation)
@@ -45,6 +44,7 @@ e_DI <- 0.3   # conversion of 2I hosts by infecteds into determinate host tissue
 # reproduction rate
 ## I guess we will make these equal for now?
 r_P <- 0.5 # reproduction of parasites (note: if we want to include pathogenicity other than death, could we subtract r from the IDH pop equation?)
-r_S <- 0.5 # reproduction of new uninfected second intermediate hosts
+r_SU <- 0.05 # reproduction of new uninfected second intermediate hosts
+r_SI <- 0.05 # reproduction of new uninfecteds by INFECTED second intermediate hosts
 
 # goals: three levels of kelp input crossed with three levels of 2IH susceptibility to predation
